1e0e4a83d5d520278169c047d3f42f78e7e84c44
Ravinder <108238004+yadavravi1610@users.noreply.github.com>
2023-09-30 00:25:43 +0530

    First commit created Netflix-Clone

---
+++ index.js
@@ -0,0 +1,237 @@
+// Get references to HTML elements
+const searchInput = document.getElementById('searchInput');
+const searchResults = document.getElementById('searchResults');
+const goToFavoriteMoviesBtn = document.getElementById('goToFavoriteMovies');
+
+// Event listener to navigate to favorite movies page
+goToFavoriteMoviesBtn.addEventListener('click', () => {
+    window.location.href = 'favorite_movies/favorite_movies.html';
+});
+
+const scrollDistance = 900;
+
+// Define a function to handle scrolling
+function setupScroll(containerClass, previousButtonClass, nextButtonClass) {
+    const containers = document.querySelectorAll(`.${containerClass}`);
+    const previousButtons = document.querySelectorAll(`.${previousButtonClass}`);
+    const nextButtons = document.querySelectorAll(`.${nextButtonClass}`);
+
+    containers.forEach((container, index) => {
+        const previousButton = previousButtons[index];
+        const nextButton = nextButtons[index];
+
+        nextButton.addEventListener('click', () => {
+            container.scrollBy({
+                left: scrollDistance,
+                behavior: 'smooth',
+            });
+        });
+
+        previousButton.addEventListener('click', () => {
+            container.scrollBy({
+                left: -scrollDistance,
+                behavior: 'smooth',
+            });
+        });
+    });
+}
+
+// Set up scrolling for each section
+setupScroll('trending-container', 'trending-previous', 'trending-next');
+setupScroll('netflix-container', 'netflix-previous', 'netflix-next');
+setupScroll('netflixShows-container', 'netflixShows-previous', 'netflixShows-next');
+setupScroll('top-container', 'top-previous', 'top-next');
+setupScroll('horror-container', 'horror-previous', 'horror-next');
+setupScroll('comedy-container', 'comedy-previous', 'comedy-next');
+setupScroll('action-container', 'action-previous', 'action-next');
+setupScroll('romantic-container', 'romantic-previous', 'romantic-next');
+
+// TMDB API key
+const api_Key = '4626200399b08f9d04b72348e3625f15';
+
+
+// Function to fetch and display movies or TV shows
+function fetchMedia(containerClass, endpoint, mediaType) {
+    const containers = document.querySelectorAll(`.${containerClass}`);
+    containers.forEach((container) => {
+        fetch(`https://api.themoviedb.org/3/${endpoint}&api_key=${api_Key}`)
+            .then(response => response.json())
+            .then(data => {
+                const fetchResults = data.results;
+                fetchResults.forEach(item => {
+                    const itemElement = document.createElement('div');
+                    const imageUrl = containerClass === 'netflix-container' ? item.poster_path : item.backdrop_path;
+                    itemElement.innerHTML = ` <img src="https://image.tmdb.org/t/p/w500${imageUrl}" alt="${item.title || item.name}"> `;
+                    container.appendChild(itemElement);
+
+                    itemElement.addEventListener('click', () => {
+                        const media_Type = item.media_type || mediaType
+                        window.location.href = `movie_details/movie_details.html?media=${media_Type}&id=${item.id}`;
+                    });
+                });
+
+                if (containerClass === 'netflix-container') {
+                    const randomIndex = Math.floor(Math.random() * fetchResults.length);
+                    const randomMovie = fetchResults[randomIndex];
+                    
+                    const banner = document.getElementById('banner');
+                    const play = document.getElementById('play-button');
+                    const info = document.getElementById('more-info');
+                    const title = document.getElementById('banner-title');
+
+                    banner.src = `https://image.tmdb.org/t/p/original/${randomMovie.backdrop_path}`;
+                    title.textContent = randomMovie.title || randomMovie.name;
+                    
+                    function redirectToMovieDetails() {
+                        const media_Type = randomMovie.media_type || mediaType;
+                        window.location.href = `movie_details/movie_details.html?media=${media_Type}&id=${randomMovie.id}`;
+                    }
+                    
+                    play.addEventListener('click', redirectToMovieDetails);
+                    info.addEventListener('click', redirectToMovieDetails);
+                }
+            })
+            .catch(error => {
+                console.error(error);
+
+            });
+    })
+}
+
+
+// Initial fetch of trending, Netflix, top rated, horror, comedy, action, and romantic on page load
+fetchMedia('trending-container', 'trending/all/week?');
+fetchMedia('netflix-container', 'discover/tv?with_networks=213', 'tv');
+fetchMedia('netflixShows-container', 'discover/tv?', 'tv');
+fetchMedia('top-container', 'movie/top_rated?', 'movie');
+fetchMedia('horror-container', 'discover/movie?with_genres=27', 'movie');
+fetchMedia('comedy-container', 'discover/movie?with_genres=35', 'movie');
+fetchMedia('action-container', 'discover/movie?with_genres=28', 'movie');
+fetchMedia('romantic-container', 'discover/movie?with_genres=10749', 'movie');
+
+
+
+// Variables to handle favorite movies list
+const favoriteMovies = JSON.parse(localStorage.getItem('favoriteMovies')) || [];
+
+// Event listener for search input changes
+searchInput.addEventListener('input', async () => {
+    const query = searchInput.value;
+    if (query.length > 2) {
+        const results = await fetchSearchResults(query);
+        if (results.length !== 0) {
+            searchResults.style.visibility = "visible";
+        }
+        displaySearchResults(results);
+    } else {
+        searchResults.innerHTML = '';
+        searchResults.style.visibility = "hidden";
+    }
+});
+
+// Event listener for Enter key press in search input
+searchInput.addEventListener('keyup', async event => {
+    if (event.key === 'Enter') {
+        const query = searchInput.value;
+        if (query.length > 2) {
+            const results = await fetchSearchResults(query);
+            if (results.length !== 0) {
+                searchResults.style.visibility = "visible";
+            }
+            displaySearchResults(results);
+        } else {
+            searchResults.innerHTML = '';
+            searchResults.style.visibility = "hidden";
+        }
+    }
+});
+
+// Function to fetch search results from OMDB API
+async function fetchSearchResults(query) {
+    const response = await fetch(`https://api.themoviedb.org/3/search/multi?api_key=${api_Key}&query=${query}`);
+    const data = await response.json();
+    return data.results || [];
+}
+
+// Function to display search results
+function displaySearchResults(results) {
+    console.log(results)
+    // Clear previous search results
+    searchResults.innerHTML = '';
+
+    // Loop through results and create HTML elements
+    results.map(movie => {
+        const shortenedTitle = movie.title || movie.name;
+        const date = movie.release_date || movie.first_air_date;
+
+        let buttonText = "Add to WatchList"; // Set default button text
+
+        // Check if the movie is already in WatchList
+        if (favoriteMovies.find(favoriteMovie => favoriteMovie.id === movie.id)) {
+            buttonText = "Go to WatchList"; // Change button text
+        }
+
+        const movieItem = document.createElement('li');
+
+        // Create HTML structure for each movie
+        movieItem.innerHTML = `
+                                <div class = "search-item-thumbnail">
+                                    <img src ="https://image.tmdb.org/t/p/w500${movie.poster_path}">
+                                </div>
+                                <div class ="search-item-info">
+                                    <h3>${shortenedTitle}</h3>
+                                    <p>${movie.media_type} <span> &nbsp; ${date}</span></p>
+                                </div>
+                                <button class="favoriteBtn" id="${movie.id}">${buttonText}</button>   
+                                `;
+
+        const favoriteBtn = movieItem.querySelector('.favoriteBtn');
+
+        // Add event listener for favorite button
+        favoriteBtn.addEventListener('click', () => {
+            if (buttonText === "Add to WatchList") {
+                addToFavorites(movie);
+            } else {
+                window.location.href = 'favorite_movies/favorite_movies.html'; // Navigate to the favorite movies page
+            }
+        });
+
+
+        const thumbnail = movieItem.querySelector('.search-item-thumbnail');
+        const info = movieItem.querySelector('.search-item-info');
+
+        // Add event listener to navigate to movie details page
+        (thumbnail && info).addEventListener('click', () => {
+            window.location.href = `movie_details/movie_details.html?media=${movie.media_type}&id=${movie.id}`;
+        });
+
+        // Append movie item to search results
+        searchResults.appendChild(movieItem);
+    });
+}
+
+// Function to add a movie to favorites
+function addToFavorites(movie) {
+    // Check if the movie is not already in the favorites list
+    if (!favoriteMovies.find(favoriteMovie => favoriteMovie.id === movie.id)) {
+        favoriteMovies.push(movie);
+        localStorage.setItem('favoriteMovies', JSON.stringify(favoriteMovies)); // Store in Local Storage
+        const favoriteBtn = document.querySelector(`[id="${movie.id}"]`);
+        if (favoriteBtn) {
+            favoriteBtn.textContent = "Go to WatchList";
+            favoriteBtn.addEventListener('click', () => {
+                window.location.href = 'favorite_movies/favorite_movies.html'; // Navigate to the favorite movies page
+            });
+        }
+    }
+}
+
+// Event listener to close search results when clicking outside
+document.addEventListener('click', event => {
+    if (!searchResults.contains(event.target)) {
+        searchResults.innerHTML = '';
+        searchResults.style.visibility = "hidden";
+    }
+});
+
+
---
+++ movie_details/movie_details.js
@@ -0,0 +1,104 @@
+// Selecting the logo element and adding a click event listener to navigate to the homepage
+const logo = document.querySelector('.logo');
+logo.addEventListener('click', () => {
+    window.location.href = '../index.html';
+});
+
+// Selecting various elements on the page for displaying movie details
+const movieTitle = document.getElementById('movieTitle');
+const moviePoster = document.getElementById('moviePoster');
+const movieYear = document.getElementById('movieYear');
+const rating = document.getElementById('rating');
+const genre = document.getElementById('genre');
+const plot = document.getElementById('plot');
+const language = document.getElementById("language");
+const iframe = document.getElementById("iframe");
+const favoriteBtn = document.querySelector('.favoriteBtn');
+const favoriteMovies = JSON.parse(localStorage.getItem('favoriteMovies')) || [];
+
+// API key for TMDB API
+const api_Key = '4626200399b08f9d04b72348e3625f15';
+
+// Retrieve the IMDb ID from the URL parameter
+const params = new URLSearchParams(window.location.search);
+const id = params.get('id');
+const media = params.get("media");
+
+// Function to fetch detailed information about the movie using its IMDb ID
+async function fetchMovieDetails(id) {
+    const response = await fetch(`https://api.themoviedb.org/3/${media}/${id}?api_key=${api_Key}`);
+    const data = await response.json();
+    return data;
+}
+
+// Display the movie details on the page
+async function displayMovieDetails() {
+    try {
+        const movieDetails = await fetchMovieDetails(id);
+
+        var spokenlanguage = movieDetails.spoken_languages.map(language => language.english_name)
+        var genreNames = movieDetails.genres.map(genre => genre.name);
+
+        // Setting the values of various elements with movie details
+        movieTitle.textContent = movieDetails.name || movieDetails.title;
+        moviePoster.src = `https://image.tmdb.org/t/p/w500${movieDetails.backdrop_path}`;
+        movieYear.textContent = `${movieDetails.release_date || movieDetails.first_air_date}`;
+        rating.textContent = movieDetails.vote_average;
+        genre.innerText = genreNames.join(', ');
+        plot.textContent = movieDetails.overview.substring(0,590);
+        language.textContent = spokenlanguage.join(', ');
+
+        // Updating the favorite button text and adding a click event listener to toggle favorites
+        if (favoriteMovies.some(favoriteMovie => favoriteMovie.id === movieDetails.id)) {
+            favoriteBtn.textContent = "Remove From WatchList";
+        } else {
+            favoriteBtn.textContent = "Add To WatchList";
+        }
+        favoriteBtn.addEventListener('click', () => toggleFavorite(movieDetails));
+
+    } catch (error) {
+        movieTitle.textContent = "Details are not available right now! Please try after some time."
+    }
+
+    try{
+        const videoDetails = await fetchVideoDetails(id);
+        console.log("videoDetails",videoDetails)
+        const trailer = videoDetails.find(video => video.type === 'Trailer');
+        console.log("trailer",trailer)
+        
+        if (trailer) {
+            iframe.src = `https://www.youtube.com/embed/${trailer.key}?autoplay=1`;
+            moviePoster.style.display="none";
+        } else {
+            iframe.style.display="none";
+        }
+    }catch(error){
+        iframe.style.display="none";
+    }
+}
+
+// Function to toggle adding/removing from favorites
+function toggleFavorite(movieDetails) {
+    const index = favoriteMovies.findIndex(movie => movie.id === movieDetails.id);
+    if (index !== -1) {
+        favoriteMovies.splice(index, 1);
+        favoriteBtn.textContent = "Add To WatchList";
+    } else {
+        favoriteMovies.push(movieDetails);
+        favoriteBtn.textContent = "Remove From WatchList";
+    }
+    localStorage.setItem('favoriteMovies', JSON.stringify(favoriteMovies));
+}
+
+// Call the function to display movie details when the page loads
+window.addEventListener('load', () => {
+    displayMovieDetails();
+});
+
+
+// Function to fetch video details (trailers) for a movie or TV show
+async function fetchVideoDetails(id) {
+    const response = await fetch(`https://api.themoviedb.org/3/${media}/${id}/videos?api_key=${api_Key}`);
+    const data = await response.json();
+    return data.results;
+}
